8f79052d025e16df5cdfadd0ecd0beb74fdc5d76
Justin Hunter <justin.d.hunter@gmail.com>
2012-05-17 09:54:15 -0400

    add attributes and helper methods

--- lib/DBIx/IntrospectDBH.pm
+++ lib/DBIx/IntrospectDBH.pm
@@ -7,8 +7,49 @@ use DBI::Const::GetInfoType ();
 
 exports qw(introspect);
 
+has dbh => (
+  is => 'ro',
+  required => 1,
+);
+
+has driver => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->dbh->{Driver} },
+);
+
+has engine => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { 'foo' }
+);
+
+has separator => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->_dbh_get_info('SQL_QUALIFIER_NAME_SEPARATOR') },
+);
+
+has server_version => (
+  is => 'ro',
+  lazy => 1,
+  default => sub { $_[0]->_dbh_get_info('SQL_DBMS_VER') },
+);
+
+sub _dbh_get_info {
+  my ($self, $info) = @_;
+
+  if ($info =~ /[^0-9]/) {
+    $info = $DBI::Const::GetInfoType::GetInfoType{$info};
+    die "Info type '$_[1]' not provided by DBI::Const::GetInfoType" unless defined $info;
+  }
+
+  return try { $self->dbh->get_info($info) } || undef;
+}
+
 sub introspect {
-  print "foobar\n";
+#  Moo::Role->create_class_with_roles(__PACKAGE__, qw(DBIx::IntrospectDBH::Test))->new( dbh => shift );
+   DBIx::IntrospectDBH->new( dbh => shift );
 }
 
 1;
