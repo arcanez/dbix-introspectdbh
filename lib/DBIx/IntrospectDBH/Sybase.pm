e353008487e33e7fdb3127f25369fa3721b5044a
Justin Hunter <justin.d.hunter@gmail.com>
2012-05-20 21:04:19 -0400

    strip down Sybase.pm

--- lib/DBIx/IntrospectDBH/Sybase.pm
+++ lib/DBIx/IntrospectDBH/Sybase.pm
@@ -1,17 +1,10 @@
 package DBIx::IntrospectDBH::Sybase;
 use Moo::Role;
 
-has server_version => (
-  is => 'ro',
-  lazy => 1,
-  default => sub { 'sybase' },
-);
-
 around BUILD => sub {
   my $orig = shift;
   my $self = shift;
-Moo::Role->apply_roles_to_object($self, qw(DBIx::IntrospectDBH::Sybase::ASE));
-  warn 'foobar';
+  #Moo::Role->apply_roles_to_object($self, qw(DBIx::IntrospectDBH::Sybase::ASE));
   $self->$orig(@_);
 };
 
