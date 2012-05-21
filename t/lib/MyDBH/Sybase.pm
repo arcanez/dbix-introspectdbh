9837dfec868017ca1fc3b127a9841fc47fe6d29e
Justin Hunter <justin.d.hunter@gmail.com>
2012-05-21 14:41:55 -0400

    add selectrow_arrayref

--- t/lib/MyDBH/Sybase.pm
+++ t/lib/MyDBH/Sybase.pm
@@ -3,11 +3,14 @@ package MyDBH::Sybase;
 sub new {
   my $self = { Driver => { Name => 'Sybase' } };
   bless $self;
-  return $self;
 }
 
 my %INFO = ( 18 => '4.9' );
 
 sub get_info { $INFO{$_[1]} }
+
+sub selectrow_arrayref {
+  return [ 'foo', 'bar', 'Sybase' ];
+}
 
 1;
