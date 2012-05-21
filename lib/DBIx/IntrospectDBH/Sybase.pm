52aff62a86b4eab3d18f82bba2aaee2811b45bad
Justin Hunter <justin.d.hunter@gmail.com>
2012-05-21 14:41:10 -0400

    figure out which role to apply

--- lib/DBIx/IntrospectDBH/Sybase.pm
+++ lib/DBIx/IntrospectDBH/Sybase.pm
@@ -1,10 +1,18 @@
 package DBIx::IntrospectDBH::Sybase;
 use Moo::Role;
+use Try::Tiny;
 
 around BUILD => sub {
   my $orig = shift;
   my $self = shift;
-  #Moo::Role->apply_roles_to_object($self, qw(DBIx::IntrospectDBH::Sybase::ASE));
+  my $dbh = $self->dbh;
+  my $dbtype = @{$dbh->selectrow_arrayref()}[2];
+  if ($dbtype) {
+    $dbtype =~ s/\W/_/gi;
+    $dbtype = 'ASE' if $dbtype eq 'SQL_Server';
+    my $role = "DBIx::IntrospectDBH::Sybase::$dbtype";
+    try { Moo::Role->apply_roles_to_object($self, $role); }
+  }
   $self->$orig(@_);
 };
 
